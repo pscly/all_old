bf7d92fdd50af8f654666ffe2687bfb9d6a3bdc5
Pscly <pscly@outlook.com>
2020-11-30 19:34:41 +0800

    更新了一下install_zsh.py

--- 02install_zsh/install_zsh.py
+++ 02install_zsh/install_zsh.py
@@ -4,6 +4,33 @@
 # 创建日期: 
 # version: 
 
+def huanyuan():
+    pass
+
+def install_git():
+    pass
+
+def install_zsh():
+    pass
+
+def install_plugins():
+    pass
+
+def runs():
+    pass
+
+dakai = {
+    '1': [huanyuan, '换源'],
+    '2': [install_git, '安装git'],
+    '3': [install_zsh, '安装zsh'],
+    '4': [install_plugins, '安装zsh插件'],
+    '5': [runs, '输入一堆，然后按顺序执行'],
+    '6': [exit, '退出'],
+}
+
+def dayin():
+    for i in dakai:
+        print(i,' \t'.expandtabs(6),dakai[i][1])
 
 
 
